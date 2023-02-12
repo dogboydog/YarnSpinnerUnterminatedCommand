ab50936d17c213b0d20d2bac4a91094838f24f45
dogboydog <dogboydog@users.noreply.github.com>
2023-02-12 10:25:28 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Yarn.Compiler;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var yarnScriptPath = Path.GetFullPath("../../../Hello.yarn");
+        var job = CompilationJob.CreateFromFiles(new List<string>
+        {
+            yarnScriptPath
+        });
+        var compilationResult = Yarn.Compiler.Compiler.Compile(job);
+        foreach (var diag in compilationResult.Diagnostics)
+        {
+            Console.WriteLine($"Diag {diag.Severity} {diag.Message}");
+        }
+    }
+}
